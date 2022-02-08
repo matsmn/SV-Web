7bd614e6efec0ec2882ba8d3c7ea66f38ac30074
matsmn <matiasayala22@protonmail.com>
2022-02-08 00:25:57 -0300

    --all

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
-    const menu_btn = document.querySelector('.btn-dropdown');
+    /* const menu_btn = document.querySelector('.btn-dropdown'); */
+    const menu_btn = document.querySelector('.hamburger');
     const sidebar = document.querySelector('.sideMenu');
 
     menu_btn.addEventListener('click', function () {
